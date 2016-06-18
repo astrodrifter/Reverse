5cbdaaab99f36755a105d7cd03c361832079b2d4
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2016-06-18 20:03:46 +1000

    Added a working solution
    This code reverses two doubles in an array. I had an error without using
    "static' in the method heading. I don't know how to get around that.

---
+++ Reverse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Reverse {
+    class Program {
+        static void Main(string[] args) {
+            double[] data = { 8.5, 12.0, 23.2, 18.0, 15.5, 5.0, 10.5 };
+            Reverse(data, 2, 5);
+            foreach(double element in data) {
+                Console.Write(element + " ");
+            }          
+            Console.ReadKey();
+        }
+        public static void Reverse(double[] values, int start, int finish) {
+            double firstNum = values[start]; // copy first positions element
+            values[start] = values[finish]; //Assign to second element
+            values[finish] = firstNum; // assign to first element.
+        }
+    }
+}
