0d704d7e55d3244d11b9a501ff9168d671b14839
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2016-06-20 00:18:24 +1000

    Now using System.Array.Reverse
    Didn't realise the questions asked to reverse element 2-5 not swap them.
    Code has been fixed.

--- Reverse/Program.cs
+++ Reverse/Program.cs
@@ -3,16 +3,14 @@ namespace Reverse {
     class Program {
         static void Main(string[] args) {
             double[] data = { 8.5, 12.0, 23.2, 18.0, 15.5, 5.0, 10.5 };
-            Reverse(data, 2, 5);
+            Reverse(data, 2, 4); // in question it says 2 and 5.
             foreach(double element in data) {
                 Console.Write(element + " ");
             }          
             Console.ReadKey();
         }
         public static void Reverse(double[] values, int start, int finish) {
-            double firstNum = values[start]; // copy first positions element
-            values[start] = values[finish]; //Assign to second element
-            values[finish] = firstNum; // assign to first element.
+            Array.Reverse(values, start, finish);
         }
     }
 }
---
+++ ReverseUsingArrayClass/ReverseUsingArrayClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseUsingArrayClass {
+    class ReverseUsingArrayClass {
+        static void Main(string[] args) {
+
+        }
+    }
+}
